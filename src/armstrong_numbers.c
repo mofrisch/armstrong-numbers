459b24b4070be52c30e63200d00aa2823d687c12
Moritz Frisch <mo68@posteo.net>
2020-12-16 15:43:37 +0100

    initial release

---
+++ src/armstrong_numbers.c
@@ -0,0 +1,25 @@
+#include "armstrong_numbers.h"
+#include <math.h>
+#include <stdio.h>
+
+int num_digits(int i) { return (int)floor(log10(i)) + 1; }
+
+bool is_armstrong_number(int candidate) {
+  int n = num_digits(candidate);
+  int sum = 0;
+  int local_candidate = candidate;
+  while (local_candidate > 0) {
+    sum += pow(local_candidate % 10, n);
+    local_candidate /= 10;
+  }
+  return candidate == sum;
+}
+
+/*
+int main() {
+  int x = 5;
+  printf("Log: %f", log10(x));
+  printf("Digits: %d", num_digits(x));
+  printf("Armstrong: %d", is_armstrong_number(x));
+}
+*/
